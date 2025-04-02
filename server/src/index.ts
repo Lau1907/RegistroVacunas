a817d17dbe733871b3413a0d0ea90778f5776f9e
Vanesa Hernández <vanehernan464775@gmail.com>
2025-04-02 15:30:20 -0600

    ahora si

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,28 @@
-import express from 'express';
+import express, { Application} from 'express';
+
+class Server {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    private config(): void {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    private routes(): void {
+        // Aquí se definirán las rutas en el futuro
+    }
+
+    public start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
+
+const server = new Server();
+server.start();
